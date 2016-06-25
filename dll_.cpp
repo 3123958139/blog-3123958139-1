9eb8a03fb89bd189c9eb29796ec19eb52eec789b
GPerelman <3123958139@qq.com>
2016-06-25 15:44:10 +0800

    by 佩雷尔曼的信徒

--- dll_.cpp
+++ dll_.cpp
@@ -3,18 +3,24 @@
 using namespace std;
 
 // c++ 结构体定义
-struct struck_ {
+struct cpp_struck_ {
 
+    // 股票代码，字符串
     char *stock_name_;
-    char **stock_date_;
 
-    double stock_open_;
-    double stock_high_;
-    double stock_low_;
-    double stock_close_;
+    // 日期，字符串数组
+    char *stock_date_[];
 
-    double stock_volume_;
+    // 开高低收四组价格，浮点型数组
+    double stock_open_[];
+    double stock_high_[];
+    double stock_low_[];
+    double stock_close_[];
 
+    // 成交量，浮点型数组
+    double stock_volume_[];
+
+    // 长度，整型
     int stock_len_;
     int ma_len_;
 };
@@ -23,7 +29,9 @@ struct struck_ {
 extern "C" {
 
 // 传入结构体指针，传出结构体指针
-struck_ *dll_function_1(struck_ *struck_pointer_) {
-    return struck_pointer_;
+cpp_struck_ *dll_function_1(cpp_struck_ *py_struck_pointer_) {
+    cpp_struck_ *cpp_struck_pointer_;
+    cpp_struck_pointer_ = py_struck_pointer_;
+    return cpp_struck_pointer_;
 }
 }
--- test_dll_.py
+++ test_dll_.py
@@ -1,22 +1,57 @@
+import tushare
 from ctypes import *
 
+# 下载测试数据
+sh_data_frame_ = tushare.get_hist_data('sh')
+stock_name_ = 'sh'
+stock_date_ = sh_data_frame_.index.values
+stock_open_ = sh_data_frame_['open'].values
+stock_high_ = sh_data_frame_['high'].values
+stock_low_ = sh_data_frame_['low'].values
+stock_close_ = sh_data_frame_['close'].values
+stock_volume_ = sh_data_frame_['volume'].values
+stock_len_ = len(sh_data_frame_)
+ma_len_ = 2
 
 # python 结构体定义
 class py_struct_(Structure):
-    _fields_ = [("stock_code_", c_char_p), ("stock_open_", c_double)]
+    _fields_ = [("stock_name_", c_char_p),
+                ("stock_date_", c_char_p * stock_len_),
+                ("stock_open_", c_double * stock_len_),
+                ("stock_high_", c_double * stock_len_),
+                ("stock_low_", c_double * stock_len_),
+                ("stock_close_", c_double * stock_len_),
+                ("stock_volume_", c_double * stock_len_),
+                ("stock_len_", c_int),
+                ("ma_len_", c_int)]
 
 
 # python 结构体实例化，初始化
 py_struct_1 = py_struct_()
-py_struct_1.stock_code_ = b"Hello world!"
-py_struct_1.stock_open_ = 123456
+py_struct_1.stock_name_ = stock_name_.encode('utf-8')
+py_struct_1.stock_open_ = (c_double * stock_len_)(*stock_open_)
 
-# 取结构体指针
+# 传入指针实例
 py_struct_1_pointer_ = byref(py_struct_1)
 
 # 获取 dll 句柄
 h_dll_ = CDLL(
     'C:\\Users\\Perelman\\.CLion2016.1\\system\\cmake\\generated\\blog-3123958139-1-6c04ac5e\\6c04ac5e\\Debug\\libdll_.dll')
 
+# 定义 dll 返回值类型为 python 结构体指针
+h_dll_.dll_function_1.restype = POINTER(py_struct_)
+
+# 返回 dll 结构体指针
+cpp_struct_pointer_ = h_dll_.dll_function_1(py_struct_1_pointer_)
+
+# 结构体指针取内容
+cpp_struct_contents_ = cpp_struct_pointer_.contents
+
 # 打印结果
-print(h_dll_.dll_function_1(py_struct_1_pointer_))
+print(cpp_struct_contents_.stock_name_.decode('utf-8'))
+
+# 保存结果为 python list 格式
+new_open_ = []
+for value in cpp_struct_contents_.stock_open_:
+    new_open_.append(value)
+print(new_open_)
