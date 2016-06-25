2f597d8c4b06922998550c91f1264ed17760ac20
GPerelman <3123958139@qq.com>
2016-06-25 14:12:32 +0800

    by 佩雷尔曼的信徒

--- dll_.cpp
+++ dll_.cpp
@@ -5,20 +5,25 @@ using namespace std;
 // c++ 结构体定义
 struct struck_ {
 
-    // 股票名，字符串
-    char *stock_code_;
+    char *stock_name_;
+    char **stock_date_;
 
-    // 开盘价
     double stock_open_;
+    double stock_high_;
+    double stock_low_;
+    double stock_close_;
+
+    double stock_volume_;
+
+    int stock_len_;
+    int ma_len_;
 };
 
 // 声明为标准 C 格式导出的函数
 extern "C" {
 
-// 参数接受结构体指针
-int dll_function_1(struck_ *struck_pointer_) {
-    cout << struck_pointer_->stock_code_ << endl;
-    cout << struck_pointer_->stock_open_ << endl;
-    return 0;
+// 传入结构体指针，传出结构体指针
+struck_ *dll_function_1(struck_ *struck_pointer_) {
+    return struck_pointer_;
 }
 }
